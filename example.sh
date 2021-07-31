1961700c7aca54e0ae64c679c14a6fa761c29f65
Thibaut <thibaut.mattio@gmail.com>
2021-07-31 13:32:03 +0200

    Update example

--- example.sh
+++ example.sh
@@ -3,4 +3,4 @@
 set -xe
 
 rm -rf example
-SPIN_CREATE_SWITCH=false SPIN_PROJECT_NAME=Demo SPIN_SYNTAX=Reason SPIN_PACKAGE_MANAGER=Esy spin new . example -d
+SPIN_CREATE_SWITCH=false SPIN_PROJECT_NAME=Demo SPIN_SYNTAX=Reason SPIN_PACKAGE_MANAGER=Esy spin new . example -d -vv
